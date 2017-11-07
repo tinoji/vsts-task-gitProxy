9ef6576b71c1fdc28af74ca83c228bbdc3340f53
tinoji <pot5.sunen.on.the.earth.614@gmail.com>
2017-11-07 12:08:13 +0900

    Init

---
+++ setGitHttpsProxy/ps_modules/VstsTaskSdk/README.md
@@ -0,0 +1,5 @@
+# VstsTaskSdk
+fetched from https://github.com/Microsoft/vsts-task-lib
+
+# memo
+remove and use git clone
---
+++ setGitHttpsProxy/setProxy.ps1
@@ -0,0 +1,15 @@
+[CmdletBinding()]
+param ()
+
+try {
+    $proxy = Get-VstsInput -Name 'ProxyConfig' -Require
+
+    # git config
+    git config --global https.sslVerify true
+    git config --global http.sslVerify true
+    git config --global https.proxy $proxy
+    git config --global http.proxy $proxy
+
+} finally {
+    Trace-VstsLeavingInvocation $MyInvocation
+}
